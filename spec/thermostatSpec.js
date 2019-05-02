2f204a09362c20948ca7df3047932c514588b16e
Oliver Cripps <ollycripps@gmail.com>
2019-05-02 16:52:56 +0100

    implement max temp error if powersave mode is on

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -26,4 +26,25 @@ describe('thermostat', function() {
     expect(thermostat.MIN_TEMP).toEqual(10);
   });
 
+  it('power save mode is on by default', function() {
+    expect(thermostat.powerSave).toBe(true);
+  });
+
+  it('maximum temperature in powersave mode equals 25', function() {
+    expect(thermostat.PS_ON_MAX_TEMP).toEqual(25);
+  });
+
+  it('maximum temperature in normal mode equals 32', function() {
+    expect(thermostat.PS_OFF_MAX_TEMP).toEqual(32);
+  });
+
+  it('when PS mode is on, the temp cannot be raised above 25', function() {
+    for (var i = 0; i <= 4; i++) {
+      thermostat.increaseTemp();
+      console.log(thermostat.currentTemp);
+      console.log(i);
+    };
+    expect(function() {thermostat.increaseTemp()}).toThrow('Max temp reached');
+  });
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -4,10 +4,17 @@ function Thermostat() {
   this.DEFAULT_TEMP = 20;
   this.currentTemp = this.DEFAULT_TEMP;
   this.MIN_TEMP = 10;
+  this.powerSave = true;
+  this.PS_ON_MAX_TEMP = 25;
+  this.PS_OFF_MAX_TEMP = 32;
 };
 
 Thermostat.prototype.increaseTemp = function () {
-  this.currentTemp++
+  if (this.powerSave === true && this.currentTemp === 25) {
+    throw ("Max temp reached");
+  } else {
+    this.currentTemp++;
+  }
 };
 
 Thermostat.prototype.decreaseTemp = function () {
